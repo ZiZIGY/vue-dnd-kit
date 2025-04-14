058b26a4f942884eb5826c34523078e66ee40283
ZiZIGY <zizigydev@gmail.com>
2025-04-14 03:29:03 +0300

    backed table in app.vue

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,46 +1,54 @@
-<script setup lang="ts">
+<script setup>
   import { ref } from 'vue';
-  import {
-    Table,
-    TableHead,
-    TableBody,
-    Draggable,
-  } from '@vue-dnd-kit/components';
+  import { Table, TableHead, TableBody } from '@vue-dnd-kit/components';
   import { DnDOperations, useDnDStore } from '@vue-dnd-kit/core';
 
-  import Drag from './components/Drag.vue';
+  const columns = ref([
+    { key: 'code', name: 'Код' },
+    { key: 'name', name: 'Название' },
+    { key: 'category', name: 'Категория' },
+    { key: 'price', name: 'Цена' },
+  ]);
 
-  // const columns = ref([
-  //   { key: 'code', name: 'Код' },
-  //   { key: 'name', name: 'Название' },
-  //   { key: 'category', name: 'Категория' },
-  //   { key: 'price', name: 'Цена' },
-  // ]);
+  const tableData = ref([
+    { code: '1001', name: 'Продукт 1', category: 'Категория A', price: 99 },
+    { code: '1002', name: 'Продукт 2', category: 'Категория B', price: 149 },
+    { code: '1003', name: 'Продукт 3', category: 'Категория A', price: 249 },
+  ]);
 
-  // const tableData = ref([
-  //   { code: '1001', name: 'Продукт 1', category: 'Категория A', price: 99 },
-  //   { code: '1002', name: 'Продукт 2', category: 'Категория B', price: 149 },
-  //   { code: '1003', name: 'Продукт 3', category: 'Категория A', price: 249 },
-  // ]);
-
-  // const formatCurrency = (value) => {
-  //   return new Intl.NumberFormat('ru-RU', {
-  //     style: 'currency',
-  //     currency: 'RUB',
-  //   }).format(value);
-  // };
+  const formatCurrency = (value) => {
+    return new Intl.NumberFormat('ru-RU', {
+      style: 'currency',
+      currency: 'RUB',
+    }).format(value);
+  };
 
   const store = useDnDStore();
 </script>
 
 <template>
-  {{ store.keyboard }}
-  <Drag
-    v-for="item in 1_000"
-    :key="item"
-  >
-    drag me
-  </Drag>
+  <Table>
+    <TableHead
+      :columns="columns"
+      @update:columns="columns = $event"
+      @drop="DnDOperations.applyTransfer"
+    />
+
+    <TableBody
+      :columns="columns"
+      :data="tableData"
+      @drop="DnDOperations.applyTransfer"
+    >
+      <template #cell="{ row, column, value }">
+        <div v-if="column.key === 'price'">
+          {{ formatCurrency(value) }}
+        </div>
+        <div v-else>
+          {{ value }}
+        </div>
+      </template>
+    </TableBody>
+  </Table>
 </template>
 
 <style>
