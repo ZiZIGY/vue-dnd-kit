cf6a1783e91749001e2c3cb5decbf7c719e46d3b
ZiZIGY <zizigydev@gmail.com>
2025-06-22 00:15:43 +0300

    fix

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -5,21 +5,6 @@ import VueDnDKitDevtools from '@vue-dnd-kit/devtools';
 import VueDnDKitPlugin, { type IPluginOptions } from '@vue-dnd-kit/core';
 import { createApp } from 'vue';
 
-// Vuetify
-import 'vuetify/styles';
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
-import '@mdi/font/css/materialdesignicons.css';
-
-const vuetify = createVuetify({
-  components,
-  directives,
-  theme: {
-    defaultTheme: 'light',
-  },
-});
-
 const app = createApp(App);
 app.use(VueDnDKitPlugin, {
   defaultOverlay: {
@@ -29,5 +14,4 @@ app.use(VueDnDKitPlugin, {
   },
 } as IPluginOptions);
 app.use(VueDnDKitDevtools);
-app.use(vuetify);
 app.mount('#app');
