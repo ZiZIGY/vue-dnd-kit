f1493b13395e0e2fb2a5851794cf1459fca44c57
ZiZIGY <zizigydev@gmail.com>
2026-04-11 14:38:35 +0300

    fix transition

--- docs/.vitepress/components/v2/Examples/OverlayTransition/TransitionOverlay.vue
+++ docs/.vitepress/components/v2/Examples/OverlayTransition/TransitionOverlay.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { makeDroppable } from '@vue-dnd-kit/core';
+import { makeDroppable, DragPreview } from '@vue-dnd-kit/core';
 </script>
 
 <template>
@@ -9,16 +9,15 @@
 </template>
 
 <style>
-  .preview-pop-enter-active {
-    transition: opacity 0.15s ease,
-      scale 0.15s cubic-bezier(0.34, 1.56, 0.64, 1);
-  }
-  .preview-pop-leave-active {
-    transition: opacity 0.1s ease, scale 0.1s ease;
-  }
-  .preview-pop-enter-from,
-  .preview-pop-leave-to {
-    opacity: 0;
-    scale: 0.85;
-  }
+.preview-pop-enter-active {
+  transition: opacity 0.15s ease, scale 0.15s cubic-bezier(0.34, 1.56, 0.64, 1);
+}
+.preview-pop-leave-active {
+  transition: opacity 0.1s ease, scale 0.1s ease;
+}
+.preview-pop-enter-from,
+.preview-pop-leave-to {
+  opacity: 0;
+  scale: 0.85;
+}
 </style>
